39cb975a58e616a248f006d001023c266d7fac5e
Wade <imbanano@gmail.com>
2015-08-30 21:40:05 +0600

    test

---
+++ src/main/java/EuclidRunner.java
@@ -0,0 +1,8 @@
+public class EuclidRunner {
+    public static void main (String[] args){
+        Euclid e = new Euclid();
+        int z;
+        z = e.euclidalgaritm(56, 788);
+        System.out.println(z);
+    }
+}
